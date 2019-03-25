f158c3509a549a388da82f387260a19f9a682400
natarajsarath <natarajsarath95@gmail.com>
2019-03-25 12:38:26 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,14 @@ import { Component } from '@angular/core';
 export class AppComponent  {
   name = 'Angular';
   appstatus:Boolean=true;
+  applist:any[]=[
+    {
+      "Id":1,
+      "Name":'Sharath'
+    },
+    {
+      "Id":2,
+      "Name":'Kumar'
+    }
+  ];
 }
