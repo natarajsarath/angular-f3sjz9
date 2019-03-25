cf9c72f836ce9d342aba95f208558b679075a44a
natarajsarath <natarajsarath95@gmail.com>
2019-03-25 15:35:08 +0530

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [BrowserModule],//used to launch the browser app
+  imports: [BrowserModule,HttpModule],//browsermodule used to launch the browser app and httpmodule is used to communicate or get the data from the backend servicea
   declarations: [AppComponent],//it tells which component belongs to which module
   bootstrap: [AppComponent]//it automatically loads the contents to the browser
 })
