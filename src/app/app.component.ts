037c345212c5eea7b5dba4ab2d17276dfb1fa81c
natarajsarath <natarajsarath95@gmail.com>
2019-03-25 14:33:14 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular';
-  appstatus:Boolean=true;
-  applist:any[]=[
+  appstatus: Boolean = true;
+  applist: any[] = [
     {
-      "Id":1,
-      "Name":'Sharath'
+      "Id": 1,
+      "Name": 'Sharath'
     },
     {
-      "Id":2,
-      "Name":'Kumar'
+      "Id": 2,
+      "Name": 'Kumar'
     }
   ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],//used to launch the browser app
-  declarations: [ AppComponent ],//it tells which component belongs to which module
-  bootstrap:    [ AppComponent ]//it automatically loads the contents to the browser
+  imports: [BrowserModule],//used to launch the browser app
+  declarations: [AppComponent],//it tells which component belongs to which module
+  bootstrap: [AppComponent]//it automatically loads the contents to the browser
 })
 export class AppModule { }
