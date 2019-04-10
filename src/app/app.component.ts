b47baef12999b6c49b4cab343f70a1c1f600a0df
natarajsarath <natarajsarath95@gmail.com>
2019-04-10 16:42:54 +0530

    2 files modified, 1 added, 2 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Student} from './Student';
 
 @Component({
   selector: 'my-app',
@@ -6,16 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = 'Angular';
-  appstatus: Boolean = true;
-  applist: any[] = [
-    {
-      "Id": 1,
-      "Name": 'Sharath'
-    },
-    {
-      "Id": 2,
-      "Name": 'Kumar'
-    }
-  ];
+  name = 'Student Marks';
+  students: Student[];
+  constructor(){
+    this.students=[
+        {"Id": 1,"Name": 'xx',"Marks":90,"Grade":"A"},
+        {"Id": 2,"Name": 'yy',"Marks":80,"Grade":"B"},
+        {"Id": 3,"Name": 'zz',"Marks":70,"Grade":"C"},
+        {"Id": 4,"Name": 'aa',"Marks":60,"Grade":"D"},
+        {"Id": 5,"Name": 'bb',"Marks":50,"Grade":"E"},
+        {"Id": 6,"Name": 'xxy',"Marks":90,"Grade":"A"},
+        {"Id": 7,"Name": 'xxz',"Marks":90,"Grade":"A"},
+        {"Id": 8,"Name": 'yya',"Marks":80,"Grade":"B"},
+        {"Id": 9,"Name": 'yyb',"Marks":80,"Grade":"B"},
+        {"Id": 10,"Name": 'zzc',"Marks":70,"Grade":"C"},
+        {"Id": 11,"Name": 'zzd',"Marks":70,"Grade":"C"},
+        {"Id": 12,"Name": 'aac',"Marks":60,"Grade":"D"},
+      ];
+  } 
 }
